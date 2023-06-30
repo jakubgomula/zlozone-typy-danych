14711e482ef75817d44a638303f5de7ad149fa58
jakubgomula <135388410+jakubgomula@users.noreply.github.com>
2023-06-30 23:37:33 +0200

    Add files via upload

---
+++ zadanie3/zadanie 3.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+public enum KlasaRzadkosci
+{
+    Powszechny,
+    Rzadki,
+    Unikalny,
+    Epicki
+}
+
+public enum TypPrzedmiotu
+{
+    Bron,
+    Zbroja,
+    Amulet,
+    Pierścień,
+    Hełm,
+    Tarcza,
+    Buty
+}
+
+public struct Przedmiot
+{
+    public double Waga;
+    public int Wartosc;
+    public KlasaRzadkosci Rzadkosc;
+    public TypPrzedmiotu Typ;
+    public string NazwaWlasna;
+
+    public Przedmiot(double waga, int wartosc, KlasaRzadkosci rzadkosc, TypPrzedmiotu typ, string nazwaWlasna)
+    {
+        Waga = waga;
+        Wartosc = wartosc;
+        Rzadkosc = rzadkosc;
+        Typ = typ;
+        NazwaWlasna = nazwaWlasna;
+    }
+
+    public void WyswietlInformacje()
+    {
+        Console.WriteLine($"Nazwa: {NazwaWlasna}");
+        Console.WriteLine($"Typ: {Typ}");
+        Console.WriteLine($"Rzadkosc: {Rzadkosc}");
+        Console.WriteLine($"Waga: {Waga}");
+        Console.WriteLine($"Wartosc: {Wartosc} sztuk złota");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Przedmiot[] przedmioty = new Przedmiot[5];
+
+        // Wypełniamy tablicę przedmiotami
+        WypelnijPrzedmioty(przedmioty);
+
+        // Wyświetlamy informacje o wszystkich przedmiotach
+        Console.WriteLine("Przedmioty:");
+        foreach (var przedmiot in przedmioty)
+        {
+            przedmiot.WyswietlInformacje();
+            Console.WriteLine("----------------------------");
+        }
+
+        // Losowo wybieramy przedmiot z uwzględnieniem rzadkości
+        Console.WriteLine("Losowo wybrany przedmiot:");
+        Przedmiot losowyPrzedmiot = WylosujPrzedmiot(przedmioty);
+        losowyPrzedmiot.WyswietlInformacje();
+    }
+
+    public static void WypelnijPrzedmioty(Przedmiot[] przedmioty)
+    {
+        przedmioty[0] = new Przedmiot(2.5, 100, KlasaRzadkosci.Powszechny, TypPrzedmiotu.Bron, "Miecz");
+        przedmioty[1] = new Przedmiot(1.8, 150, KlasaRzadkosci.Rzadki, TypPrzedmiotu.Zbroja, "Zbroja płytowa");
+        przedmioty[2] = new Przedmiot(0.3, 50, KlasaRzadkosci.Unikalny, TypPrzedmiotu.Amulet, "Amulet mocy");
+        przedmioty[3] = new Przedmiot(0.1, 200, KlasaRzadkosci.Epicki, TypPrzedmiotu.Pierścień, "Pierścień nieśmiertelności");
+        przedmioty[4] = new Przedmiot(0.8, 80, KlasaRzadkosci.Powszechny, TypPrzedmiotu.Hełm, "Hełm rycerski");
+    }
+
+    public static Przedmiot WylosujPrzedmiot(Przedmiot[] przedmioty)
+    {
+        Random rand = new Random();
+        int sumaPrawdopodobienstw = 0;
+
+        foreach (var przedmiot in przedmioty)
+        {
+            sumaPrawdopodobienstw += (int)przedmiot.Rzadkosc + 1;
+        }
+
+        int losowaWartosc = rand.Next(1, sumaPrawdopodobienstw + 1);
+        int aktualnaSuma = 0;
+
+        foreach (var przedmiot in przedmioty)
+        {
+            aktualnaSuma += (int)przedmiot.Rzadkosc + 1;
+            if (losowaWartosc <= aktualnaSuma)
+            {
+                return przedmiot;
+            }
+        }
+
+        // W razie jakiegoś problemu zwracamy pierwszy przedmiot z tablicy
+        return przedmioty[0];
+    }
+}
---
+++ zadanie4/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+public enum Plec
+{
+    Mezczyzna,
+    Kobieta
+}
+
+public struct Student
+{
+    public string Nazwisko;
+    public int NrAlbumu;
+    public double Ocena;
+    public Plec Plec;
+
+    public Student(string nazwisko, int nrAlbumu, double ocena, Plec plec)
+    {
+        Nazwisko = nazwisko;
+        NrAlbumu = nrAlbumu;
+        Ocena = Math.Max(2.0, Math.Min(5.0, ocena)); // Ograniczenie oceny do przedziału [2.0, 5.0]
+        Plec = plec;
+    }
+
+    public void WyswietlInformacje()
+    {
+        Console.Write($"{Nazwisko} (Nr albumu: {NrAlbumu}), ");
+        Console.Write($"Ocena: {Ocena}, ");
+        Console.WriteLine($"Płeć: {Plec}");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Student[] grupa = new Student[5];
+
+        // Wypełniamy tablicę informacjami o studentach
+        WypelnijStudentow(grupa);
+
+        // Wyświetlamy informacje o studentach
+        Console.WriteLine("Studenci:");
+        foreach (var student in grupa)
+        {
+            student.WyswietlInformacje();
+        }
+
+        // Obliczamy i wyświetlamy średnią ocen
+        double sredniaOcen = ObliczSredniaOcen(grupa);
+        Console.WriteLine($"Średnia ocen: {sredniaOcen}");
+    }
+
+    public static void WypelnijStudentow(Student[] grupa)
+    {
+        grupa[0] = new Student("Kowalski", 123456, 4.5, Plec.Mezczyzna);
+        grupa[1] = new Student("Nowak", 234567, 3.0, Plec.Mezczyzna);
+        grupa[2] = new Student("Wójcik", 345678, 2.5, Plec.Kobieta);
+        grupa[3] = new Student("Lewandowska", 456789, 5.5, Plec.Kobieta); // Ocena zostanie ograniczona do 5.0
+        grupa[4] = new Student("Jankowski", 567890, 1.0, Plec.Mezczyzna); // Ocena zostanie ograniczona do 2.0
+    }
+
+    public static double ObliczSredniaOcen(Student[] grupa)
+    {
+        double sumaOcen = 0;
+
+        foreach (var student in grupa)
+        {
+            sumaOcen += student.Ocena;
+        }
+
+        return sumaOcen / grupa.Length;
+    }
+}
